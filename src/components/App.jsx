4ecb98899983778d2f2bdc829c43e537cf522ff9
Michal Leszczyk <Michal Leszczyk>
2023-10-03 21:35:32 +0200

    remade with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,131 +1,108 @@
-import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Modal } from './Modal/Modal';
 import { Loader } from './Loader/Loader';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
+import { useState, useEffect } from 'react';
 
 const APIkey = '39764230-ed321e5fcf0eec48c182a2af6';
 
-export class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    query: '',
-    isLoading: false,
-    isModal: false,
-    perPage: 12,
-  };
-  showLoadMore = false;
-  modalData = {};
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModal, setIsModal] = useState(false);
+  const [modalData, setModalData] = useState({});
+  const [showLoadMore, setShowLoadMore] = useState(false);
+  const perPage = 12;
 
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    const query = e.target.search.value;
-
-    if (query) {
-      this.setState(prevState => {
-        if (prevState.query !== query) {
-          return {
-            query: query,
-            page: 1,
-            images: [],
-          };
-        }
-      });
+    const newQuery = e.target.search.value;
+    if (newQuery) {
+      if (query !== newQuery) {
+        setQuery(newQuery);
+        setPage(1);
+        setImages([]);
+      }
     }
   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.query !== this.state.query
-    ) {
-      this.setState({ isLoading: true });
-      const { query, page, perPage } = this.state;
+  useEffect(() => {
+    const fetchData = async () => {
+      if (page === 1) {
+        setImages([]);
+      }
+      if (query.trim() === '') {
+        setShowLoadMore(false);
+        return;
+      }
+      setIsLoading(true);
       try {
         const response = await fetch(
           `https://pixabay.com/api/?q=${query}&page=${page}&key=${APIkey}&image_type=photo&orientation=horizontal&per_page=${perPage}`
         );
         if (!response.ok) {
-          throw new Error(`error!`);
+          throw new Error('Error!');
         }
         const responseData = await response.json();
         if (!responseData.totalHits) {
-          this.showLoadMore = false;
-
+          setShowLoadMore(false);
           alert('There are no images matching your search query.');
         } else {
-          if (this.state.page === 1) {
-            this.setState({ images: [...responseData.hits] });
+          if (page === 1) {
+            setImages([...responseData.hits]);
           } else {
-            this.setState({
-              images: [...prevState.images, ...responseData.hits],
-            });
+            setImages(prevImages => [...prevImages, ...responseData.hits]);
           }
 
-          if (perPage * this.state.page >= responseData.totalHits) {
-            this.showLoadMore = false;
+          if (perPage * page >= responseData.totalHits) {
+            setShowLoadMore(false);
           } else {
-            this.showLoadMore = true;
+            setShowLoadMore(true);
           }
         }
-      } catch (error) {
-        this.showLoadMore = false;
+      } catch (e) {
+        setShowLoadMore(false);
         alert('Error. Please try again.');
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
-    }
-  }
-
-  onClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-  onClickImage = e => {
-    document.addEventListener('keydown', this.keyDown);
-    this.modalData = {
-      img: e.target.dataset.bgimage,
-      alt: e.target.dataset.alt,
     };
-    this.setState({
-      isModal: !this.state.isModal,
-    });
+    fetchData();
+  }, [query, page]);
+
+  const onClick = () => {
+    setPage(page + 1);
   };
-  keyDown = e => {
+  const onClickImage = e => {
+    document.addEventListener('keydown', keyDown);
+    setModalData({ img: e.target.dataset.bgimage, alt: e.target.dataset.alt });
+    setIsModal(!isModal);
+  };
+
+  const keyDown = e => {
     if (e.code === 'Escape') {
-      document.removeEventListener('keydown', this.keyDown);
-      this.setState({
-        isModal: !this.state.isModal,
-        modalData: {},
-      });
+      document.removeEventListener('keydown', keyDown);
+      setIsModal(false);
+      setModalData({});
     }
   };
-  closeModal = e => {
+  const closeModal = e => {
     if (e.target === e.currentTarget) {
-      document.removeEventListener('keydown', this.keyDown);
-      this.setState({
-        isModal: !this.state.isModal,
-        modalData: {},
-      });
+      document.removeEventListener('keydown', keyDown);
+      setIsModal(!isModal);
+      setModalData({});
     }
   };
-  render() {
-    const { images, isLoading } = this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallery images={images} onClickImage={this.onClickImage} />
-        {images[0] && this.showLoadMore && !isLoading && (
-          <Button onClick={this.onClick} />
-        )}
-        {this.state.isLoading && <Loader />}
-        {this.state.isModal && (
-          <Modal data={this.modalData} closeModal={this.closeModal} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Searchbar onSubmit={onSubmit} />
+      <ImageGallery images={images} onClickImage={onClickImage} />
+      {images[0] && showLoadMore && !isLoading && <Button onClick={onClick} />}
+      {isLoading && <Loader />}
+      {isModal && <Modal data={modalData} closeModal={closeModal} />}
+    </div>
+  );
+};
